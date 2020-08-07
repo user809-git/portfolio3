b81829137aac188e6205e0d2a7bda927249716a3
user809-git <64389223+user809-git@users.noreply.github.com>
2020-08-07 11:04:00 +0900

    807 update

---
+++ sub/js/software.js
@@ -0,0 +1,51 @@
+$(function(){
+    
+    let windowH = $(window).innerHeight(),
+        scrollY = $(window).scrollTop();
+    let innerImg = [
+        {
+            box: $(".sec1 .inner").eq(0).children(".innerImg"),
+            img: $(".sec1 .inner").eq(0).children(".innerImg").children("img"),
+            top: '',
+            end: ''
+        },
+        {
+            box: $(".sec1 .inner").eq(1).children(".innerImg"),
+            img: $(".sec1 .inner").eq(1).children(".innerImg").children("img"),
+            top: '',
+            end: ''
+        },
+        {
+            box: $(".sec1 .inner").eq(2).children(".innerImg"),
+            img: $(".sec1 .inner").eq(2).children(".innerImg").children("img"),
+            top: '',
+            end: ''
+        }
+    ];
+    
+    (function setOffset() {
+        for (let i = 0; i < innerImg.length; i++) {
+            innerImg[i].top = innerImg[i].box.offset().top;
+            innerImg[i].end = innerImg[i].box.offset().top + innerImg[i].box.innerHeight();
+        }
+    })();
+    
+    $(window).scroll(function(){
+        
+        scrollY = $(window).scrollTop();
+        let scrollRatio;
+        let value = 0;
+        scrollRatio = scrollY / windowH;
+        value = scrollRatio * 5 - 2.5;
+        if (scrollY+windowH >= innerImg[0].top && scrollY <= innerImg[0].end) {
+            innerImg[0].img.css({ 'top': `${2.5 - value}%` });
+        }
+        if (scrollY+windowH >= innerImg[1].top && scrollY <= innerImg[1].end) {
+            innerImg[1].img.css({ 'top': `${value}%` });
+        }
+        if (scrollY+windowH >= innerImg[2].top && scrollY <= innerImg[2].end) {
+            innerImg[2].img.css({ 'top': `${value}%` });
+        }
+    });
+    
+});
